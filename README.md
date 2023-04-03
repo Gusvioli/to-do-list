9ea7d8168166f6fbcd7fae4bfa35387a1ebabb8e
Gusvioli <gustavovieiradeoliveira@gmail.com>
2023-04-03 14:10:17 -0300

    Algumas refatoraçoes nos componentes

--- README.md
+++ README.md
@@ -5,8 +5,78 @@
 - [x] Criar um componente para rodapé
 - [x] Correção de bugs( Logotipo quebrando )
 - [ ] Sistema para recuperar senha
-- [ ] Correção de sinconização de estado com UseQuery e Banco de dados
-- [ ] Melhorar a exibição do nome do usuário natela
-- [ ] Adicionar um pequeno botão de fechamento do campo de pesquisa
-- [ ] Adicionar um titulo. exemplo: "Faça seu login no sistema" no campo de login
-- [ ] Adicionar um titulo. exemplo: "Faça seu cadastro no sistema" no campo de cadastro
+- [x] Melhorar a exibição do nome do usuário na tela home
+- [x] Adicionar um titulo. exemplo: "Faça seu login no sistema" no campo de login
+- [x] Adicionar um titulo. exemplo: "Faça seu cadastro no sistema" no campo de cadastro
+
+# Testes para o Frontend
+
+## Testes em Páginas
+
+### Pagina Home
+
+- [ ] Teste se os itens estão sendo renderizados na tela
+
+### Pagina login
+
+- [ ] Teste se os itens estão sendo renderizados na tela
+- [ ] Teste se a navegação entre as tela de login e cadastro está funcionando
+- [ ] Teste se a navegação vai direto para home, quando o usuário estiver logado
+- [ ] Teste se o usuário consegue realizar o login
+- [ ] Teste se o login não é realizado com dados incorretos
+- [ ] Teste com usuário inexistente
+- [ ] Teste com senha incorreta
+- [ ] Teste com dados incorretos
+- [ ] Teste com dados vazios
+- [ ] Teste se apôs o login os dados do usuário estão sendo armazenados corretamente
+- [ ] Teste se o usuário é redirecionado para a tela home, após fazer login
+- [ ] Teste se o campo de senha fica vazio apos o login do usuário ser sucedido
+- [ ] Teste se ao tentar realizar o login, o botão fica desabilitado
+- [ ] Teste se os dados da tela de login estão sendo enviados para o banco de dados
+
+### Pagina Register
+
+- [ ] Teste se os itens estão sendo renderizados na tela
+- [ ] Teste se a navegação entre as tela de cadastro e login está funcionando
+- [ ] Teste se a navegação vai direto para login, quando o usuário for cadastrado
+- [ ] Teste se o usuário consegue realizar o cadastro
+- [ ] Teste se o cadastro não é realizado com dados inválidos
+- [ ] Teste com usuário em branco
+- [ ] Teste com senha em branco
+- [ ] Teste com E-mail em branco
+- [ ] Teste com dados inválidos
+- [ ] Teste se apôs o cadastro os dados do usuário estão sendo armazenados corretamente
+- [ ] Teste se o usuário é redirecionado para tela home, após fazer login depois que foi cadastrado
+- [ ] Teste se o cadastro não é realizado quando o usuário já é cadastrado
+- [ ] Teste se o cadastro não é realizado caso o usuário já exista
+- [ ] Teste se ao tentar realizar o cadastro, o botão fica desabilitado
+- [ ] Teste se os dados da tela de cadastro estão sendo enviados para o banco de dados
+- [ ] Teste se o usuário é notificado quando o login já foi utilizado
+
+## Testes em Componentes
+
+### Componente Apis
+
+- [ ] Teste se a função GitHubApi está retornando um valor válido
+- [ ] Teste se a função RequestDataContentsApi esta retornando um valor válido
+
+### Componente Buttons
+
+- [ ] Teste se o botão BtnCadastro está sendo renderizado
+- [ ] teste se o botão BtnLogin está sendo renderizado
+
+### Componente Calendar
+
+- [ ] Teste se o componente ListCalendar está sendo renderizado
+- [ ] Teste se o conteudo renderizado pelo componente ListCalendar está sendo apresentado
+- [ ] Teste se o componente PanelCalendar está sendo renderizado
+  
+### Componente Control
+### Componente Create
+### Componente Emojis
+### Componente Footer
+### Componente search
+### Componente Simple
+### Componente typesToDoList
+### Componente Navbar
+### Componente NavbarAdmin
--- app/frontend/src/components/Navbar.tsx
+++ app/frontend/src/components/Navbar.tsx
@@ -4,11 +4,13 @@ import Context from "../context/Context";
 import '../styles/pages/navbar.css';
 import { hendleClearAll } from "./utils/clears/HendleclearAll";
 import { useQueryClient } from "react-query";
+import TypesToDoList from "./typesToDuList/TypesToDoList";
 // import LogoToDoList from '../../assets/images/logoToDoList.png';
 
 function Navbar(): JSX.Element {
   const {
     setSearch,
+    search,
     setNameEmojiUrl,
     setFormCreateAndEditTask,
     setEmail,
@@ -56,44 +58,54 @@ function Navbar(): JSX.Element {
             title="Logotipo to do list"
           />
           <h2>To do list</h2>
-        </section>
-        <section className="nav-section-2">
-          <form className="nav-section-2-form">
-            <label htmlFor="buscador">
-              <input
-                data-testid='input-buscador'
-                type="text"
-                id="buscador"
-                title="Search by: #id, description, date, emoji"
-                placeholder={
-                  history.location.pathname.split('/')[2] === 'calendar'
-                  ? 'Search by: #id, description, date, emoji in calendar'
-                  : 'Search by: #id, description, date, emoji in simple'
-                }
-                size={45}
-                onChange={ (e) => hendleSearch(e) }
-              />
-            </label>
-            {
-              dataTokenUserName?.token
-              ?
-                <img
-                  style={{ cursor: 'pointer' }}
-                  width={25}
-                  height={25}
-                  src="https://github.githubassets.com/images/icons/emoji/unicode/274c.png?v8"
-                  alt="Button Exit site"
-                  title="Exit site"
-                  onClick={ hendleExited }
+          <section className="nav-section-2">
+            <form className="nav-section-2-form">
+              <label htmlFor="buscador">
+                <input
+                  data-testid='input-buscador'
+                  className="input-buscador"
+                  type="text"
+                  id="buscador"
+                  title="Search by: #id, description, date, emoji"
+                  placeholder={
+                    history.location.pathname.split('/')[2] === 'calendar'
+                    ? 'Search by: #id, description, date, emoji in calendar'
+                    : 'Search by: #id, description, date, emoji in simple'
+                  }
+                  value={ search }
+                  onChange={ (e) => hendleSearch(e) }
                 />
-              : ''
-            }
-          </form>
+              </label>
+            </form>
+          </section>
+        <TypesToDoList />
         </section>
-        <section data-testid='user-name'>
+        <section
+          data-testid='user-name'
+          className="name-user"
+        >
           {
             dataTokenUserName?.nameUser.name
-            ? <span title="Your name"> Hi, {dataTokenUserName?.nameUser.name}</span>
+            ? <span title="Your name" className="name-exibitions">
+                <b>
+                  Hi,
+                </b>
+                {dataTokenUserName?.nameUser.name}
+              </span>
+            : ''
+          }
+          {
+            dataTokenUserName?.token
+            ?
+              <img
+                style={{ cursor: 'pointer' }}
+                width={20}
+                height={20}
+                src="https://github.githubassets.com/images/icons/emoji/unicode/274c.png?v8"
+                alt="Button Exit site"
+                title="Exit site"
+                onClick={ hendleExited }
+              />
             : ''
           }
         </section>
