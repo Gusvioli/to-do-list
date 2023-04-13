bcf6a418773fadb1872c0874942809c963f52303
Gusvioli <gustavovieiradeoliveira@gmail.com>
2023-04-13 08:31:00 -0300

    Alteração de alguns arquivos de testes

---
+++ app/frontend/src/tests/home/units/home.test.jsx
@@ -0,0 +1,11 @@
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../../../pages/Home';
+
+describe('Testando a página Home', () => {
+  test('Deve renderizar o título da página', () => {
+    render(<Home />);
+    const screenHome = screen.getByTestId('form-Login');
+    expect(screenHome).toBeInTheDocument();
+  });
+});
